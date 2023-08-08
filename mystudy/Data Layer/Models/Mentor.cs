9d63000f9ac5e48013567e7d85b5e3481bc39e93
OzodovGitHub <mygithub2003@mail.ru>
2023-08-08 18:10:51 +0300

    I formatted my Mentor class code

--- mystudy/Data Layer/Models/Mentor.cs
+++ mystudy/Data Layer/Models/Mentor.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Data_Layer.Models
 {
     public class Mentor
     {
         public int MentorID { get; set; }
-        [MaxLength(2)]
+        
         public int MentorAge { get; set; }
+
         public string MentorName { get; set; }
+
         public string MentorEmail { get; set; }
+
         public string MentorPhone { get; set; }
+
         public string MentorCourse { get; set; }
+
         public string CourseName { get; set; }
+
         public string MentorLevel { get; set; }
+
         public string StudentName { get; set; }
-        public string StudentEmail { get; set;}
+
+        public string StudentEmail { get; set; }
     }
 }
