9d08a1d965ff7fff5cfb5f3613670b25718264fa
Habib-Gulomov <gulomovhabibulloh@gmail.com>
2023-08-08 18:33:37 +0500

    clean up the student model

--- mystudy/Data Layer/Models/Group.cs
+++ mystudy/Data Layer/Models/Group.cs
@@ -1,20 +1,23 @@
-﻿
-
-
-namespace Data_Layer.Models
+﻿namespace Data_Layer.Models
 {
     public class Group
     {
         public Guid GroupId { get; set; }
+
         public string GroupoName { get; set; }
+
         public string GroupTime { get; set; }
 
         public string MentorName { get; set; }
+
         public Mentor Mentor { get; set; }
+
         public string StudentName { get; set; }
+
         public Student Student { get; set; }
 
         public string CourseName { get; set; }
+
         public Course Courses { get; set; }
     }
 }
