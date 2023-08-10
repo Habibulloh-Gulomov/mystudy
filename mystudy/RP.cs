5cf6036fe2b39f413fa97efde4ef30970c33cbd8
Lycoris <lycoris9188@gmail.com>
2023-08-10 16:22:38 +0500

    Implementation of patterns

---
+++ mystudy/RP.cs
@@ -0,0 +1,46 @@
+﻿public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _dbSet.AsEnumerable();
+    }
+
+    public T GetById(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
+
---
+++ mystudy/UoW.cs
@@ -0,0 +1,56 @@
+﻿public interface IUnitOfWork
+{
+    IRepository<Student> Students { get; }
+    IRepository<Mentor> Mentors { get; }
+    IRepository<Course> Courses { get; }
+    void Commit();
+    void SaveChanges();
+}
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+    private IRepository<Student> _studentRepository;
+    private IRepository<Mentor> _mentorRepository;
+    private IRepository<Course> _courseRepository;
+
+    public UnitOfWork(DbContext context)
+    {
+        _context = context;
+    }
+
+    public IRepository<Student> Students
+    {
+        get
+        {
+            return _studentRepository ??= new Repository<Student>(_context);
+        }
+    }
+
+    public IRepository<Mentor> Mentors
+    {
+        get
+        {
+            return _mentorRepository ??= new Repository<Mentor>(_context);
+        }
+    }
+
+    public IRepository<Course> Courses
+    {
+        get
+        {
+            return _courseRepository ??= new Repository<Course>(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _dbContext.SaveChanges();
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+}
+
