9bc520851f46c27db6fb2c908f2cf8dfd656bbb0
Habib-Gulomov <gulomovhabibulloh@gmail.com>
2023-08-12 20:24:04 +0500

    studentresponseDTO and studentrequestDT) created

---
+++ mystudy/mystudy.Service/dto/StudentResponseDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO
+{
+    internal class StudentResponseDTO
+    {
+        [Required]
+        public string StudentName { get; set; }
+
+        [Required]
+        public int StudentAge { get; set; }
+
+        public string StudentGender { get; set; }
+
+        [Required]
+        [MinLength(9)]
+        [MaxLength(9)]
+        public int StudentPhone { get; set; }
+
+        public string StudentEmail { get; set; }
+    }
+}
