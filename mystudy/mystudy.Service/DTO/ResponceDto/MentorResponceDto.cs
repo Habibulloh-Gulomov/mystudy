0c146af5ac29942c0e89512bb459bc070da12d41
OzodovGitHub <mygithub2003@mail.ru>
2023-08-15 18:11:47 +0300

    Add Req and Res Dto Mentor

---
+++ mystudy/mystudy.Service/DTO/ResponceDto/MentorResponceDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO.ResponceDto
+{
+    internal class MentorResponceDto
+    {
+        [Required]
+        public int MentorID { get; set; }
+        [Required]
+        public int MentorAge { get; set; }
+        [Required]
+        public string MentorName { get; set; }
+        [Required]
+        public string MentorEmail { get; set; }
+        [Required]
+        public string MentorPhone { get; set; }
+        [Required]
+        public string MentorCourse { get; set; }
+        [Required]
+        
+        public string MentorLevel { get; set; }    
+        
+    }
+}
