d86c6c02d01e95b92542a0f0dafff7daf9662e64
saidjonsaidov07 <saidjonsaidov707@gmail.com>
2023-08-17 09:02:04 -0500

    update-student-model

--- mystudy/Data Layer/Models/Student.cs
+++ mystudy/Data Layer/Models/Student.cs
@@ -13,11 +13,13 @@ namespace Data_Layer.Models
 
         public string Gender { get; set; }
 
-        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
 
         public string Email { get; set; }
 
         public bool isPaid { get; set; }
+
+        public Guid CourseId { get; set; } 
 
         public Course Courses { get; set; }
     }
