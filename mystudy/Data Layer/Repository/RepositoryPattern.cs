fe8450e5c5669fadd795ca6d1119ca92103c4aee
Lycoris <lycoris9188@gmail.com>
2023-08-30 19:32:03 +0500

    Fixed Repository Pattern

---
+++ mystudy/Data Layer/Repository/RepositoryPattern.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ mystudy/Data Layer/Repository/UnityOfWork.cs
@@ -0,0 +1,58 @@
+﻿using Data_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private IRepository<Student> _studentRepository;
+        private IRepository<Mentor> _mentorRepository;
+        private IRepository<Course> _courseRepository;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Student> Students
+        {
+            get
+            {
+                return _studentRepository ??= new Repository<Student>(_context);
+            }
+        }
+
+        public IRepository<Mentor> Mentors
+        {
+            get
+            {
+                return _mentorRepository ??= new Repository<Mentor>(_context);
+            }
+        }
+
+        public IRepository<Course> Courses
+        {
+            get
+            {
+                return _courseRepository ??= new Repository<Course>(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
